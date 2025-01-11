b1113dbe6f7e0e39da5e867358188e43b8084966
danilopereira8 <danilosantosadm95@gmail.com>
2025-01-11 09:58:18 -0300

    removendo o build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',  // Define a pasta de saída para 'build'
+    outDir: 'dist',  // Define a pasta de saída para 'build'
   },
 })
 
