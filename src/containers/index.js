d666e8a3cbf66d9cc9cad947224b0edc35e06ccd
danilopereira8 <danilosantosadm95@gmail.com>
2025-01-11 00:05:46 -0300

    importação

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,6 @@
 export * from './Cart'
 export * from './Home'
-export * from './Login/index.jsx';
+export * from './Login';
 export * from './Menu'
 export * from './Register'
 export * from './Checkout'
