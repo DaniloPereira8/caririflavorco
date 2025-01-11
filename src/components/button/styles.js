21101e093156716dd8e264742990316207f505ee
danilopereira8 <danilosantosadm95@gmail.com>
2025-01-11 15:25:05 -0300

    styles

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -1,24 +1,3 @@
-// import styled from "styled-components";
-
-
-// export const ContainerButton = styled.button`
-// width: 100%;
-// height: 52px;
-// border: none;
-// border: 5px;
-// background-color: ${(props) => props.theme.purple};
-// font-family: 'Road Rage', sans-serif ;
-// font-size: 30px;
-// color: ${(props)=>props.theme.white};
-
-// &:hover {
-
-// background-color: ${(props) => props.theme.secondDarkPurple};
-// border: 3px dashed ${(props)=>props.theme.white};
-// border-radius: 5px;
-// }
-// `
-
 import styled from "styled-components";
 
 export const ContainerButton = styled.button`
--- src/containers/Home/styles.js
+++ src/containers/Home/styles.js
@@ -1,39 +1,3 @@
-// import styled from 'styled-components';
-// import BannerHome from '../../assets/home.png';
-// import Background from '../../assets/Background.svg';
-
-// export const Banner = styled.div`
-// background: url('${BannerHome}');
-// background-size: cover;
-// background-position: center;
-// height: 480px;
-
-// h1 {
-//     font-family: 'Road Rage', sans-serif;
-//     font-size: 80px;
-//     color: ${(props) => props.theme.darkWhite};
-//     position: absolute;
-//     left: 0;
-//     padding-left: 100px;
-//     top: 30%;
-// }
-// `;
-
-
-// export const Container = styled.section`
-// background: linear-gradient(
-//     rgba(255,255,255, 0.6),
-//     rgba(255,255,255, 0.6)
-// ),
-
-// url('${Background}');
-// height: 800px;
-// `;
-
-// // export const Content = styled.div``;
-// //ficou como div, mas se for usar precisa alterar para content no index
-
-
 import styled from 'styled-components';
 import BannerHome from '../../assets/home.png';
 import Background from '../../assets/background.svg';
