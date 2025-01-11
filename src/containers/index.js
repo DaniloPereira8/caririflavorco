ae037187ba99eb40f8b22b437ab4c65dcf90b77d
danilopereira8 <danilosantosadm95@gmail.com>
2025-01-11 00:21:02 -0300

    export login

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,6 @@
 export * from './Cart'
 export * from './Home'
-export * from './Login';
+export { default as Login } from './Login';
 export * from './Menu'
 export * from './Register'
 export * from './Checkout'
